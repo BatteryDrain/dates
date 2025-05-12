232bf0b730a5fb3f63dfd59bec3b1e77631da6f9
BatteryDrain <115777815+BatteryDrain@users.noreply.github.com>
2025-05-12 08:18:28 -0600

    added days untill

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 DATE = [
     "7/4/2004",
     
-    ]
+    ];
 TITLE = [
     "Evan's birthday",
 
-    ]
+    ];
 TAG = [
     "birthday",
-]
+];
 MONTHS = [
     "January",
     "February",
@@ -22,13 +22,19 @@ MONTHS = [
     "October",
     "November",
     "December"
-]
+];
 const currentDate = new Date();
 console.log(currentDate);
-let currentDay = currentDate.getDay();
+let currentDay = currentDate.getDate();
 let currentMonth = currentDate.getMonth();
-let currentYear = currentDate. getFullYear();
+let currentYear = currentDate.getFullYear();
 current.innerHTML = currentDay + " of " + MONTHS[currentMonth] + " of " + currentYear;
+
+daysuntil = [];
+
+for(i = 0; i < DATE.length; i++){
+
+}
 
 buildcontent();
 
@@ -39,19 +45,24 @@ function buildcontent() {
 }
 
 function figbuilder(index){
-    let [day, month, year] = DATE[index].split("/");
+    let [day, month, year] = DATE[index].split("/").map(Number);
+    then = new Date (currentYear,month - 1,day);
+    then.setHours(0, 0, 0, 0);
+    diffTime = then - currentDate;
+    diffTime = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+    daysuntil.push(diffTime);
+
     fig = document.createElement('figure');
     fig.setAttribute("data-tag", TAG[index]);
     fig.setAttribute("data-day", day);
     fig.setAttribute("data-month", month);
 
-
     title = document.createElement('figcaption');
-    title.innerHTML = day + " of " + MONTHS[parseInt(month)];
+    title.innerHTML = day + " of " + MONTHS[parseInt(month) - 1];
     fig.appendChild(title);
 
     description = document.createElement('p');
-    description.innerHTML = TITLE[index];
+    description.innerHTML = "There are " + diffTime + " days until " + TITLE[index];
     fig.appendChild(description);
 
     content.appendChild(fig);
