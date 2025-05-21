67294bb43586d5910bec96ec99c5de38251025f2
BatteryDrain <115777815+BatteryDrain@users.noreply.github.com>
2025-05-21 13:06:06 -0600

    adjusted name confusion

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ DATE = [
     ["15/6","father's day","event"],
     ["28/6", "Paul W's birthday","birthday"],
     ["14/7/2007","Thomas W's birthday","birthday"],
-    ["2/11/2003","Paul's birthday","birthday"],
+    ["2/11/2003","Paul C's birthday","birthday"],
     ["20/12", "Emma's birthday", "birthday"],
     ["25/12","christmas", "event"]
     ];
